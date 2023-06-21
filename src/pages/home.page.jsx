c1eac096673e9def4c7abb98637effb43898d66d
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-06-21 20:11:12 +0700

    add navbar and add input form to search film

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import NavbarComponent from '../components/navbar.component';
 
 const HomePage = () => {
+  const [query, setQuery] = useState('');
+
+  console.info(query, '=> yang anda cari');
+
   return (
     <>
-      <h1>Halaman home</h1>
+      {/* Navbar Component */}
+      <NavbarComponent />
+
+      <div className="container mt-3">
+        {/* Search Bar */}
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search Film..."
+            onChange={ (e) => setQuery(e.target.value) }
+            value={ query }
+          />
+          <button
+            className="btn btn-primary"
+            type="button"
+            id="button-addon2"
+          >
+            Button
+          </button>
+        </div>
+      </div>
     </>
   );
 }

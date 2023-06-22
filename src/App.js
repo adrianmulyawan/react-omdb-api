8f0c71f8382dfa5ce3c704a3e648c694945f17cf
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-06-22 12:47:23 +0700

    add route to not found page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from "./pages/home.page";
+import NotfoundPage from "./pages/notfound.page";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={ <HomePage /> } />
+        <Route path="*" element={ <NotfoundPage /> } />
       </Routes>
     </BrowserRouter>
   );

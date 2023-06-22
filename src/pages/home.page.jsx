ad37197899342fc4910feec2492086a5152ad132
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-06-22 13:07:21 +0700

    show film

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -1,10 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import NavbarComponent from '../components/navbar.component';
+import axios from 'axios';
 
 const HomePage = () => {
   const [query, setQuery] = useState('');
+  const [film, setFilm] = useState([]);
 
-  console.info(query, '=> yang anda cari');
+  useEffect(() => {
+    const searchFilm = async () => {
+      const response = await axios.get(`https://www.omdbapi.com/?s=${query}&apikey=9775ea2d`);
+      const data = response.data;
+      setFilm(data.Search);
+    }
+    searchFilm();
+  }, [query]);
 
   return (
     <>
@@ -28,6 +37,39 @@ const HomePage = () => {
           >
             Button
           </button>
+        </div>
+
+        {/* Film */}
+        <div className="row align-items-center">
+          {
+            film && film.map((data) => {
+              return (
+                <div className="col-sm-6 col-md-4 col-lg-4 my-2" key={ data.imdbID }>
+                  <div className="card" style={{ borderRadius: '15px' }}>
+                    <img src={ data.Poster || process.env.PUBLIC_URL + "/movie.jpg" }
+                    className="card-img-top" 
+                    alt="film-banner" 
+                    style={{ width: '100%', height: '75vh', borderTopLeftRadius: '15px', borderTopRightRadius: '15px' }} 
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">
+                        { data.Title } ({ data.Year })
+                      </h5>
+                      <p className="card-text">
+                        Release: { data.Year }
+                      </p>
+                      <p className='card-text'>
+                        Movie Type: { data.Type }
+                      </p>
+                      <div class="d-grid mt-2">
+                        <p class="btn btn-primary">Detail</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )
+            })
+          }
         </div>
       </div>
     </>
